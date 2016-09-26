80b52b9861acfde88a60a51aad3a1f52e5726abb
bato3 <bato3@bandyci.org>
2016-09-26 07:44:05 +0200

    Store IP in SESSION

--- classes/Kohana/Geoip.php
+++ classes/Kohana/Geoip.php
@@ -15,15 +15,19 @@ class Kohana_Geoip{
     
     static public function instance(){
         if(empty(self::$istance))
-            self::$istance = new Geoip();
+            self::$istance = new Geoip(true);
         return self::$istance;
     }
     
     private $reader = NULL;
     
-    protected function __construct() {
-        //use ;
-        $this->reader = new  MaxMind\Db\Reader(Kohana::$config->load('geoip.database'));
+    protected function __construct($load_reader = true) {
+        if($load_reader)
+            $this->reader = new  MaxMind\Db\Reader(Kohana::$config->load('geoip.database'));
+    }
+    
+    public static function factory_empty(){
+        return new Geoip(false);
     }
     
     public function get($ip) {
@@ -36,7 +40,56 @@ class Kohana_Geoip{
         return $this->reader->close();
     }
     
+    /**
+      * Klucz, pod jakim dane są przechowywane w sesji.
+      */
+    private function get_key($ip = NULL, $iso_code = NULL){
+        if(empty($ip))
+            $ip = $this->get_real_ip();
+        
+        if(empty($iso_code))
+            $iso_code = I18n::lang();
+        $iso_code = strtoupper($iso_code);
+        
+        return  'geoip-test'.(empty($iso_code)?'NULL':$iso_code)
+                  .'-'.(empty($ip)?'NULL':$ip);
+    }
+    
+    
+    /**
+     * Czy dane IP pochodzi z konkretnego kraju, wersja z cache.
+     */
     public function is_country($ip = NULL, $iso_code = NULL){
+        $key = $this->get_key($ip, $iso_code);
+        
+        $s = Session::instance();
+        
+        $val = $s->get($key);
+        
+        if($val === NULL){
+            $val = $this->_is_country($ip, $iso_code);
+            $s->set($key, $val);
+        }
+        
+        return $val;
+    }
+    
+    /**
+     * Czy dane IP pochodzi z konkretnego kraju, wersja BEZ cache.
+     */
+    public function is_country_fresh_test($ip = NULL, $iso_code = NULL){
+        $key = $this->get_key($ip, $iso_code);
+        $val = $this->_is_country($ip, $iso_code);
+        $s = Session::instance();
+        $s->set($key, $val);
+        
+        return $val;
+    }
+    
+    /**
+     * Czy dane IP pochodzi z konkretnego kraju.
+     */
+    private function _is_country($ip = NULL, $iso_code = NULL){
         if(empty($ip))
             $ip = $this->get_real_ip();
         
@@ -73,18 +126,21 @@ class Kohana_Geoip{
     
     public function get_real_ip() {
         if (isset($_SERVER)) {
-            if (isset($_SERVER["HTTP_X_FORWARDED_FOR"])) {
+            if (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+                    $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+            }
+            else if (isset($_SERVER["HTTP_X_CLIENT_IP"])) {
+                $ip = $_SERVER["HTTP_X_CLIENT_IP"];
+            }
+            else if (isset($_SERVER["HTTP_CLIENT_IP"])) {
+                $ip = $_SERVER["HTTP_CLIENT_IP"];
+            }
+            else if (isset($_SERVER["HTTP_X_FORWARDED_FOR"])) {
                 $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
                 if (strpos($ip, ",")) {
                     $exp_ip = explode(",", $ip);
                     $ip = $exp_ip[0];
                 }
-            }
-            else if (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) {
-                    $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
-            }
-            else if (isset($_SERVER["HTTP_CLIENT_IP"])) {
-                $ip = $_SERVER["HTTP_CLIENT_IP"];
             }
             else if (isset($_SERVER['HTTP_FROM']) 
                 && filter_var($_SERVER['HTTP_FROM'], FILTER_VALIDATE_IP)) {
@@ -111,6 +167,8 @@ class Kohana_Geoip{
             }
         }
     
+        if(!filter_var($ip, FILTER_VALIDATE_IP))
+            return '::1';
         return $ip;
     }
 }
